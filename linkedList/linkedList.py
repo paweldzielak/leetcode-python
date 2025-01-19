ee98e1728c2fbb55aee378d866ed574d2c96140d
Paweł Dziełak <pawel.programowanie@hotmail.com>
2025-01-19 17:30:33 +0100

    linkedList import

---
+++ linkedList/linkedList.py
@@ -0,0 +1,19 @@
+from typing import Optional, List
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        return "ListNode(val=" + str(self.val) + ", next={" + str(self.next) + "})"
+
+
+def list_to_LL(arr):
+    if len(arr) < 1:
+        return None
+
+    if len(arr) == 1:
+        return ListNode(arr[0])
+    return ListNode(arr[0], next=list_to_LL(arr[1:]))
